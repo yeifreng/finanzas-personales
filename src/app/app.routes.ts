a16d2725b4086f4df404ae06d4a45350abb9350d
Yeifreng <yeifrenjose@gmail.com>
2025-05-16 07:51:34 -0500

    feat: maquetando modulo de ingreso, gasto y categoria sin dividir por componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,18 @@ export const routes: Routes = [
     loadComponent: () => import('./dashboard/pages/dashboard/dashboard.component')
   },
   {
+    path:'ingreso',
+    loadComponent: () => import('./income/pages/income/income.component')
+  },
+  {
+    path:'categoria',
+    loadComponent: () => import('./category/pages/category/category.component')
+  },
+  {
+    path:'gasto',
+    loadComponent: () => import('./expense/pages/expense/expense.component')
+  },
+  {
     path:'**',
     loadComponent:() => import('./dashboard/pages/dashboard/dashboard.component')
   }
---
+++ src/app/expense/pages/expense/expense.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expense',
+  standalone: true,
+  imports: [],
+  templateUrl: './expense.component.html',
+  styleUrl: './expense.component.css'
+})
+export default class ExpenseComponent {
+
+}
